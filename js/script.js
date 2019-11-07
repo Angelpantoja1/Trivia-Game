651d6098693e8ffc31d8a8d5534e0dd6a9a9247c
Code <code@COCTL-41291.local>
2019-11-06 16:02:15 -0800

    almost linked

---
+++ js/script.js
@@ -0,0 +1,330 @@
+//Retrieve html elements
+let inject = document.getElementById('inject');
+
+
+
+
+
+
+// function loadInstructions(){
+//     let xmlhttp = new XMLHttpRequest();
+//     let url = "../site/instructions.html";
+
+//     xmlhttp.onreadystatechange = function() {
+//         if (this.readyState == 4 && this.status == 200) {
+//             let myArr = this.responseText;//JSON.parse(this.responseText);
+//             console.log(myArr);
+
+//             page1Load(myArr);
+//         }
+//     };
+//     //opens connection
+//     xmlhttp.open("GET", url, true);
+
+//     //pulls the request
+//     xmlhttp.send();
+// }
+function injectHtml(url) {
+    let xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            let myArr = this.responseText;//JSON.parse(this.responseText);
+            if (url === '../site/mainMenu.html') {
+                loadMainMenu(myArr);
+            } else if (url === '../site/options.html') {
+                loadOptions(myArr);
+            } else if (url === '../site/instructions.html') {
+                loadInstructions(myArr);
+            } else if (url === '../site/lvlSelect.html') {
+                loadLvl(myArr);
+            } else if (url === '../site/borderlands.html') {
+                loadBld1(myArr);
+            } else if (url === '../site/borderlands2.html') {
+                loadBld2(myArr);
+            } else if (url === '../site/borderlands3.html') {
+                loadBld3(myArr);
+            }
+
+        }
+    };
+    //opens connection
+    xmlhttp.open("GET", url, true);
+
+    //pulls the request
+    xmlhttp.send();
+}
+
+function page1Load(info) {
+    //going to load page 1 html elements and click events
+    inject.innerHTML = info;
+
+}
+
+function page2Load(info) {
+    inject.innerHTML = info;
+}
+
+
+let bl1Data = [];
+let bl2Data = [];
+let bl3Data = [];
+
+function loadTriviaData(url) {
+    let xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            let myArr = JSON.parse(this.responseText);
+            questionInfo(myArr);
+        }
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+    function questionInfo(info) {
+        for (let i = 0; i < info.borderlands1.length; i++) {
+            //create an object for each loop in array
+            let nFO = {
+                "Q": info.borderlands1[i].Q,
+                "a1": info.borderlands1[i].a1,
+                "a2": info.borderlands1[i].a2,
+                "a3": info.borderlands1[i].a3,
+                "a4": info.borderlands1[i].a4,
+                "c": info.borderlands1[i].c
+            };
+            bl1Data.push(nFO);
+        }
+        for (let i = 0; i < info.borderlands2.length; i++) {
+            //create an object for each loop in array
+            let nFO = {
+                "Q": info.borderlands2[i].Q,
+                "a1": info.borderlands2[i].a1,
+                "a2": info.borderlands2[i].a2,
+                "a3": info.borderlands2[i].a3,
+                "a4": info.borderlands2[i].a4,
+                "c": info.borderlands2[i].c
+            };
+            bl2Data.push(nFO);
+        }
+    }
+}
+
+
+function setClass(classString) {
+    questions.className = classString;
+    answer1.className = classString;
+    answer2.className = classString;
+    answer3.className = classString;
+    answer4.className = classString;
+    correct.className = classString;
+}
+
+//load mainmenu
+
+function loadMainMenu(info) {
+    inject.innerHTML = info;
+    let injectLvl = document.getElementById('injectLvl');
+    let injectOptions = document.getElementById('injectOptions');
+    injectLvl.addEventListener('click', function (e) {
+        //call loadJSON to inject HTML
+        injectHtml('../site/instructions.html');
+    });
+    injectOptions.addEventListener('click', function (e) {
+        //call loadJSON to inject HTML
+        injectHtml('../site/options.html');
+    });
+}
+
+function loadOptions(info) {
+    inject.innerHTML = info;
+    let funLoad = document.getElementById('funLoad')
+    let exitLoad = document.getElementById('exitLoad')
+    let backLoad = document.getElementById('backLoad')
+
+    funLoad.addEventListener('click', function () {
+        injectHtml("http://csh.bz/line/05xp.html");
+    });
+    exitLoad.addEventListener('click', function () {
+        injectHtml("https://www.google.com/");
+    });
+    backLoad.addEventListener('click', function () {
+        injectHtml("../site/mainMenu.html");
+    });
+
+}
+function loadInstructions(info) {
+    inject.innerHTML = info;
+    let ready = document.getElementById('ready');
+
+    ready.addEventListener('click', function () {
+        injectHtml('../site/lvlSelect.html');
+    });
+}
+function loadLvl(info) {
+    inject.innerHTML = info;
+    let injectBld = document.getElementById('injectBld');
+    let injectBld2 = document.getElementById('injectBld2');
+    let injectBld3 = document.getElementById('injectBld3');
+
+    injectBld.addEventListener('click', function () {
+        injectHtml('../site/borderlands.html');
+    });
+    injectBld2.addEventListener('click', function () {
+        injectHtml('../site/borderlands2.html');
+    });
+    injectBld3.addEventListener('click', function () {
+        injectHtml('../site/borderlands3.html');
+    });
+}
+
+function questionsRandomizer(q) {
+    //q is triviaQFUll
+    let rNum = 0;
+    let triviaQ = [];
+    for (let i = 0; i < 20; i++) {
+        //we are going to shuffle
+        rNum = Math.floor(Math.random() * q.length);
+        //console.log(qNum);
+        //add from ezQ json array to triviaQ
+        triviaQ.push(q[rNum]);
+        //Remove the item from ezQ
+        q.splice(rNum, 1);
+    }
+    console.log(triviaQ);
+    return triviaQ;
+}
+
+function addText(obj) {
+    let questions = document.getElementById('Q');
+    let answer1 = document.getElementById('a1');
+    let answer2 = document.getElementById('a2');
+    let answer3 = document.getElementById('a3');
+    let answer4 = document.getElementById('a4');
+    let correct = document.getElementById('c');
+
+    questions.innerText = obj.Q
+    answer1.innerText = obj.a1
+    answer2.innerText = obj.a2
+    answer3.innerText = obj.a3
+    answer4.innerText = obj.a4
+    correct.innerText = obj.c
+    qCounter++;
+}
+
+
+let qCounter = 0;
+
+function loadBld1(info) {
+    inject.innerHTML = info;
+    let triviaData = questionsRandomizer(bl1Data);
+    let answer1 = document.getElementById('a1');
+    let answer2 = document.getElementById('a2');
+    let answer3 = document.getElementById('a3');
+    let answer4 = document.getElementById('a4');
+    let correct = document.getElementById('c');
+    let counter = document.getElementById('counter');
+    setInterval(checkTime, 1000);
+
+    addText(triviaData[qCounter]);
+    checkTime();
+
+    answer1.addEventListener('click', function (e) {
+        setClass("");
+        addText();
+        /* setTimeout(() => {
+            diffColor();
+        }, 100); */
+    });
+    answer2.addEventListener('click', function (e) {
+        setClass("");
+        addText();
+        /* setTimeout(() => {
+            diffColor();
+        }, 100); */
+    });
+    answer3.addEventListener('click', function (e) {
+        setClass("");
+        addText();
+        /* setTimeout(() => {
+            diffColor();
+        }, 100); */
+    });
+    answer4.addEventListener('click', function (e) {
+        setClass("");
+        addText();
+        /* setTimeout(() => {
+            diffColor();
+        }, 100); */
+    });
+    correct.addEventListener('click', function (e) {
+        setClass("");
+        addText(triviaData[qCounter]);
+        /* setTimeout(() => {
+            diffColor();
+        }, 100); */
+    });
+}
+function loadBld2(info) {
+    inject.innerHTML = info;
+    let triviaData = questionsRandomizer(bl2Data);
+    let answer1 = document.getElementById('a1');
+    let answer2 = document.getElementById('a2');
+    let answer3 = document.getElementById('a3');
+    let answer4 = document.getElementById('a4');
+    let correct = document.getElementById('c');
+    let counter = document.getElementById('counter');
+    setInterval(checkTime, 1000);
+
+    triviaTimer = 30;
+
+    addText(triviaData[qCounter]);
+    checkTime();
+
+    answer1.addEventListener('click', function (e) {
+        setClass("");
+        addText();
+        /* setTimeout(() => {
+            diffColor();
+        }, 100); */
+    });
+    answer2.addEventListener('click', function (e) {
+        setClass("");
+        addText();
+        /* setTimeout(() => {
+            diffColor();
+        }, 100); */
+    });
+    answer3.addEventListener('click', function (e) {
+        setClass("");
+        addText();
+        /* setTimeout(() => {
+            diffColor();
+        }, 100); */
+    });
+    answer4.addEventListener('click', function (e) {
+        setClass("");
+        addText();
+        /* setTimeout(() => {
+            diffColor();
+        }, 100); */
+    });
+    correct.addEventListener('click', function (e) {
+        setClass("");
+        addText(triviaData[qCounter]);
+        /* setTimeout(() => {
+            diffColor();
+        }, 100); */
+    });
+}
+//let triviaTimer = document.getElementById('counter');
+let triviaTimer = 30;
+//setInterval(checkTime, 1000);
+
+function checkTime() {
+    if (triviaTimer > 0) {
+        counter.innerText = triviaTimer--;
+    } else {
+        clearInterval(checkTime);
+    }
+}
+injectHtml("../site/mainMenu.html");
+loadTriviaData('../Data/data.json');
